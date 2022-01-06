3857652ed3661d3c0c287dafec667637cabdd8b6
mua-wallace <muawallizewi@gmail.com>
2022-01-06 01:31:25 +0100

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    // addProduct function
+    function addProduct(Request $req) {
+        $product =new Product;
+        $product->name = $req->input('name');
+        $product->category = $req->input('category');
+        $product->description = $req->input('description');
+        $product->price = $req->input('price');
+        $product->quantity = $req->input('quantity');
+        $product->file_path = $req->file('image')->store('products');
+        $product->delivery_date = $req->input('delivery_date');
+        $product->save();
+        return  $product;
+    }
+    // list all products function
+    function list() {
+        return Product::all();
+    }
+    // delete function
+    function delete($id) {
+        $result = Product::where('id', $id)->delete();
+        if($result){
+            return ["result"=>"product has been deleleted"];
+        }
+        else {
+            return ["result"=>"Ooooops!!! the operation failed or product no longer exist in Database"];
+        }
+    }
+    // getProduct function to get a single product
+    function getProduct($id) {
+        return Product::find($id);
+    }
+
+    function updateProduct($id , Request $req) {
+        // return  $req->input();
+        $product = Product::find($id);
+        $product->name = $req->input('name');
+        $product->category = $req->input('category');
+        $product->description = $req->input('description');
+        $product->price = $req->input('price');
+        $product->quantity = $req->input('quantity');
+        $product->delivery_date = $req->input('delivery_date');
+        return  $product->save();
+    }
+
+    function search($key) {
+        return Product::where('name', 'Like', "%$key%")->get();
+    }
+
+
+}
