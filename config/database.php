80db7341d0b067084e63b6a0a9d0979ae28394d9
mua-wallace <muawallizewi@gmail.com>
2022-01-07 12:11:43 +0100

    modififed DATABASE_URL

--- config/database.php
+++ config/database.php
@@ -67,8 +67,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL('host'),
-            'port' => $DATABASE_URL('port'),
+            'host' =>isset($DATABASE_URL['host']) ? $DATABASE_URL['host'] : null,
+            'port' => $DATABASE_URL['port'],
             'database' => ltrim($DATABASE_URL['path'], '/'),
             'username' => $DATABASE_URL['user'], 
             'password' => $DATABASE_URL['pass'], 
